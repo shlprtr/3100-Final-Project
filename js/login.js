1c0b2eedc4e608ede870e7b2d394d154a5728ebf
Emma Long <emmag2623@gmail.com>
2025-03-16 19:49:32 -0400

    add input validation for login/registration using sweetalert2

--- js/login.js
+++ js/login.js
@@ -1,7 +1,37 @@
 // login
-// document.querySelector('#btnLogin').addEventListener('click', (event) => {
+document.querySelector('#btnLogin').addEventListener('click', (event) => {
+    const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
 
-// })
+    const strEmail = document.querySelector('#txtEmail').value.trim().toLowerCase()
+    const strPassword = document.querySelector('#txtPassword').value
+
+    let blnError = false
+    let strError = ""
+
+    if (!regEmail.test(strEmail)) {
+        blnError = true
+        strError += "<p class='mb-0 mt-0'>Must enter a valid email</p>"
+    } 
+
+    if (strPassword.length < 1) {
+        blnError = true
+        strError += "<p class='mb-0 mt-0'>Password cannot be blank</p>"
+    }
+
+    if (blnError) {
+        Swal.fire({
+            title: 'Oh no, an error occurred!',
+            html: strError,
+            icon: 'error'
+        })
+    } else {
+        Swal.fire({
+            title: 'Success!',
+            text: 'You have successfully logged in',
+            icon: 'success'
+        })
+    }
+})
 
 // swap to register component
 document.querySelector('#btnSwapLogin').addEventListener('click', (event) => {
--- js/register.js
+++ js/register.js
@@ -1,7 +1,55 @@
 // register
-// document.querySelector('#btnRegister').addEventListener('click', (event) => {
+document.querySelector('#btnRegister').addEventListener('click', (event) => {
+    const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
 
-// })
+    let strEmail = document.querySelector('#txtEmail').value.trim().toLowerCase()
+    const strFirstName = document.querySelector('#txtFirstName').value
+    const strLastName = document.querySelector('#txtLastName').value
+    const strPassword = document.querySelector('#txtPassword').value
+    const strConfirmPassword = document.querySelector('#txtConfirmPassword').value
+
+    let blnError = false
+    let strError = ""
+
+    if (!regEmail.test(strEmail)) {
+        blnError = true
+        strError += "<p class='mb-0 mt-0'>Must enter a valid email</p>"
+    }
+
+    if (strPassword.length < 1) {
+        blnError = true
+        strError += "<p class='mb-0 mt-0'>Password cannot be blank</p>"
+    }
+
+    if (strPassword !== strConfirmPassword) {
+        blnError = true
+        strError += "<p class='mb-0 mt-1'>Passwords do not match</p>"
+    }
+
+    if (strFirstName.length < 1) {
+        blnError = true
+        strError += "<p class='mb-0 mt-0'>First name cannot be blank</p>"
+    }
+
+    if (strLastName.length < 1) {
+        blnError = true
+        strError += "<p class='mb-0 mt-0'>Last name cannot be blank</p>"
+    }
+
+    if (blnError) {
+        Swal.fire({
+            title: 'Oh no, an error!',
+            html: strError,
+            icon: 'error'
+        })
+    } else {
+        Swal.fire({
+            title: 'Success!',
+            text: 'You have successfully registered',
+            icon: 'success'
+        })
+    }
+})
 
 // swap to login component
 document.querySelector('#btnSwapRegister').addEventListener('click', (event) => {
